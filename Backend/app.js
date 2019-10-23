737e888989c097127f04f4f390f4fc3c2644af25
Paul Bomett <moblminds1@gmail.com>
2019-10-23 22:28:36 +0300

    chore: setup express server and backend directory

---
+++ Backend/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+
+app.get('/', (req, res)=>{
+    res.send('INDEX');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server running on port ${PORT}`));
